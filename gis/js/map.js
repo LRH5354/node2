29789cc24e9dcb184bce22cc023074258b492322
LRH158 <1587910290@qq.com>
2018-04-22 09:45:25 +0800

    添加 gis 文件夹

--- gis/js/map.js
+++ gis/js/map.js
@@ -4,46 +4,112 @@
  **/
 
 require([
-    "esri/map", "esri/geometry/Extent",
-    "esri/layers/FeatureLayer", "esri/InfoTemplate",
-    "esri/renderers/SimpleRenderer", "dojo/_base/Color",
-    "esri/symbols/SimpleFillSymbol", "esri/symbols/SimpleLineSymbol",
+    "esri/map",
+    "esri/toolbars/draw",
+    "esri/geometry/Point",
+    "esri/symbols/SimpleMarkerSymbol",
+    "esri/symbols/SimpleFillSymbol",
+    "esri/symbols/SimpleLineSymbol",
+    "esri/symbols/PictureMarkerSymbol",
+    "esri/symbols/PictureFillSymbol",
+    "esri/symbols/CartographicLineSymbol",
+    "esri/graphic",
+    "esri/InfoTemplate",
+    "dojo/query",
+    "dojo/_base/Color",
+    "dojo/dom",
+    "dojo/on",
+    "esri/dijit/InfoWindow",
+    "esri/dijit/Popup",
+    "dojo/dom-construct",
+
     "dojo/domReady!"
-], function(dojo,Point, SimpleMarkerSymbol, Color,
-            InfoTemplate, Graphic,SimpleFillSymbol,
-            SimpleLineSymbol,Draw ) {
+], function( Map,
+             Draw,
+             Point,
+             SimpleMarkerSymbol,
+             SimpleFillSymbol,
+             SimpleLineSymbol,
+             PictureMarkerSymbol,
+             PictureFillSymbol,
+             CartographicLineSymbol,
+             Graphic,
+             InfoTemplate,
+             query,
+             Color,
+             dom,
+             on,
+          InfoWindow,
+             Popup,
+             domConstruct) {
+    console.log(window.location.host)
+
     var myMap = new esri.Map("map");
     var myTiledMapServiceLayer = new esri.layers.ArcGISDynamicMapServiceLayer("http://localhost:6080/arcgis/rest/services/map/Map_SCAU/MapServer/");
-    myMap.addLayer(myTiledMapServiceLayer);
+         myMap.addLayer(myTiledMapServiceLayer);
+
+    myMap.on('load',function () {
+
+        var toolbar = new Draw(myMap);
+
+            //toolbar.activate(Draw.POINT);
+            myMap.graphics.on("mouse-over",function (e) {
+            })
+            myMap.on('click',function (e) {
+               var pt = new Point(e.mapPoint.x,e.mapPoint.y,myMap.spatialReference)
+               var sms = new SimpleMarkerSymbol().setColor(new Color([250,0,0,0.5])).setSize(12);
+               var attr={'x':e.mapPoint.x,'y':e.mapPoint.y,'name':'测试'};
+               var info=new InfoTemplate('属性','${x}')
+               var graphic = new Graphic(pt,sms,attr,info);
+               myMap.graphics.add(graphic);
 
 
-myMap.on('load',function () {
+            });
 
-    var toolbar = new esri.toolbars.Draw(myMap);
-   var ev= toolbar.on('draw-end',function (e) {
+            myMap.graphics.on('click',function (evt) {
+                console.log(evt)
+            })
+            myMap.on('dbl-click',function (e) {
 
-        var sfs = new SimpleFillSymbol();
-        var graphic=new Graphic(e.geometry,sfs);
-        myMap.graphics.add(graphic);
+                var sfs = new SimpleFillSymbol(
+                    SimpleFillSymbol.STYLE_SOLID,
+                    new SimpleLineSymbol(
+                        SimpleLineSymbol.STYLE_SOLID,
+                        new Color([111, 0, 255]),
+                        2
+                    ),
+                    new Color([111, 0, 255, 0.15])
+                );
+                var iw = new Popup({
+                    fillSymbol: sfs
+                },domConstruct.create('div'));
+                var emailLink = domConstruct.create("a", {
+                    "class": "action",
+                    "innerHTML": "Email Map",
+                    "href": "javascript:void(0);"
+                }, query(".actionList", iw.domNode)[0]);
 
-       toolbar.deactivate();
+                iw.setMap(myMap);
+                iw.setTitle('test');
+                iw.setContent('cjjfjskd')
+                iw.show(e.mapPoint);
+                console.log(iw);
+                console.log(myMap.infoWindow)
+
+            });
+
+
+            toolbar.on('draw-end',function (e){
+
+                var pfs = new PictureMarkerSymbol('https://p.upyun.com/docs/cloud/demo.jpg', 100, 100);
+                var attr={'x':2,'y':5,'name':'测试'};
+                var info=new InfoTemplate('属性')
+                var graphic=new Graphic(e.geometry,pfs,attr).setInfoTemplate(info);
+                myMap.graphics.add(graphic);
+                toolbar.deactivate();
+            });
+
+
     })
-    myMap.on('click',function (e) {
-        toolbar.activate(esri.toolbars.Draw.EXTENT);
-       var pt = new Point(e.mapPoint.x,e.mapPoint.y,myMap.spatialReference)
-       var sms = new SimpleMarkerSymbol().setStyle(
-           SimpleMarkerSymbol.STYLE_CIRCLE).setColor(
-           new Color([255,0,0,0.5]));
-            sms.setSize(12);
-       var graphic = new Graphic(pt,sms);
-       myMap.graphics.add(graphic);
-
-    })
-    myMap.on('mouse-down',function () {
-
-    })
-
-
-})
 
 });
